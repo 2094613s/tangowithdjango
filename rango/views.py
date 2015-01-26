ba885602e8d23ded5d24aeb049be12bd559f96c6
Akos Szente <akos.szente@egroup.hu>
2015-01-26 18:17:23 +0100

    Chapter 5 - completed

--- rango/views.py
+++ rango/views.py
@@ -8,3 +8,7 @@ from django.shortcuts import render
 def index(request): 
     context_dict = {'boldmessage':"I am bold font from the context"}
     return render(request, 'rango/index.html', context_dict)
+
+def about(request): 
+    context_dict = {'about_message':"This tutorial has been put together by Akos Szente, 2094613s"}
+    return render(request, 'rango/about.html', context_dict)
