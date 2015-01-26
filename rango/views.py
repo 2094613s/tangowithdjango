4bb32edc4d19a52e90b51cf6365120278ff03803
Akos Szente <akos.szente@egroup.hu>
2015-01-26 17:20:42 +0100

    Chapter 4 - completed

---
+++ rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request): 
+    context_dict = {'boldmessage':"I am bold font from the context"}
+    return render(request, 'rango/index.html', context_dict)
